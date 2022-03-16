0acf9a2532af0dc0186d15f8d79b4ceac3f60ca5
duduvillar10 <duduvillar10io@hotmail.com>
2022-03-16 11:01:06 -0300

    authenticate middleware

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../../shared/errors/AppError';
+import { IUsersRepository } from '../../infra/repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Email or password incorrect!');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new AppError('Email or password incorrect!');
+    }
+
+    const token = sign({}, 'e253bf43a9a2fd99960537ee142fd7c1', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { assetsRoutes } from './assets.routes';
+import { authenticateRoutes } from './authenticate.routes';
 import { companiesRoutes } from './companies.routes';
 import { unitsRoutes } from './units.routes';
 import { usersRoutes } from './users.routes';
@@ -9,5 +10,6 @@ router.use('/assets', assetsRoutes);
 router.use('/units', unitsRoutes);
 router.use('/companies', companiesRoutes);
 router.use('/users', usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
