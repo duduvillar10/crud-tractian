ef1f24316b4930271a8232b9be7fc1fde8dbfbd2
duduvillar10 <duduvillar10io@hotmail.com>
2022-03-16 23:55:51 -0300

    fix: list users by company

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ListAssetsByUnitController } from '../../../../modules/assets/useCases/listAssetsByUnit/ListAssetsByUnitController';
 import { CreateUserController } from '../../../../modules/users/useCases/createUser/CreateUserController';
 import { DeleteUserController } from '../../../../modules/users/useCases/deleteUser/DeleteUserController';
+import { ListUsersByCompanyController } from '../../../../modules/users/useCases/listUsersByCompany/ListUsersByCompanyController';
 import { UpdateUserController } from '../../../../modules/users/useCases/updateUser/UpdateUserController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -10,7 +11,7 @@ const usersRoutes = Router();
 const createUserController = new CreateUserController();
 const updateUserController = new UpdateUserController();
 const deleteUserController = new DeleteUserController();
-const listUsersByCompanyController = new ListAssetsByUnitController();
+const listUsersByCompanyController = new ListUsersByCompanyController();
 
 usersRoutes.post('/', createUserController.handle);
 
